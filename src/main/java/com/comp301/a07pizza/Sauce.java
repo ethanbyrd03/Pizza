509028e4f0d87cfde8a730a5c0e7950b87a61d8d
Ethan Byrd <ethbyr@ad.unc.edu>
2023-11-01 14:38:19 -0400

    novice

--- src/main/java/com/comp301/a07pizza/Sauce.java
+++ src/main/java/com/comp301/a07pizza/Sauce.java
@@ -8,6 +8,6 @@ public class Sauce extends IngredientImpl {
     public static final Sauce TOMATO = new Sauce("tomato", true, true);
     public static final Sauce BARBECUE = new Sauce("barbecue", true, true);
     public static final Sauce PESTO = new Sauce("pesto", true, true);
-    public static final Sauce ALFREDO = new Sauce("pesto", true, false);
-    public static final Sauce RANCH = new Sauce("ranch", true, true);
+    public static final Sauce ALFREDO = new Sauce("alfredo", true, false);
+    public static final Sauce RANCH = new Sauce("ranch", true, false);
 }
