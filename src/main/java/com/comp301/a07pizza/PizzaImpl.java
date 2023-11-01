826392aa37d259e087f2b6fa4ad3f8ada082638b
Ethan Byrd <ethbyr@ad.unc.edu>
2023-11-01 16:00:10 -0400

    Jedi

--- src/main/java/com/comp301/a07pizza/PizzaImpl.java
+++ src/main/java/com/comp301/a07pizza/PizzaImpl.java
@@ -1,49 +1,91 @@
 package com.comp301.a07pizza;
 
 public class PizzaImpl implements Pizza {
+    private final Size _size;
+    private final Crust _crust;
+    private final Sauce _sauce;
+    private final Cheese _cheese;
+    private final Ingredient[] _toppings;
+
+    public PizzaImpl(Size size, Crust crust, Sauce sauce, Cheese cheese, Ingredient[] toppings) {
+        this._size = size;
+        this._crust = crust;
+        this._sauce = sauce;
+        this._cheese = cheese;
+        this._toppings = toppings;
+    }
 
     @Override
     public boolean isVegetarian() {
-        return false;
+        for (int i = 0; i < this._toppings.length; i++) {
+            if (!this._toppings[i].isVegetarian()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean isVegan() {
-        return false;
+        for (int i = 0; i < this._toppings.length; i++) {
+            if (!this._toppings[i].isVegan()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        double price = 0.00;
+        if (this._size == Size.SMALL) {
+            price += 7.00;
+            price += (this._toppings.length * 0.25);
+        } else if (this._size == Size.MEDIUM) {
+            price += 9.00;
+            price += (this._toppings.length * 0.50);
+        } else {
+            price += 11.00;
+            price += (this._toppings.length * 0.75);
+        }
+        return price;
     }
 
     @Override
     public Size getSize() {
-        return null;
+        return this._size;
     }
 
     @Override
     public Ingredient getSauce() {
-        return null;
+        return this._sauce;
     }
 
     @Override
     public Ingredient getCheese() {
-        return null;
+        return this._cheese;
     }
 
     @Override
     public Ingredient getCrust() {
-        return null;
+        return this._crust;
     }
 
     @Override
     public Ingredient[] getToppings() {
-        return new Ingredient[0];
+        return this._toppings;
     }
 
     @Override
     public Ingredient[] getIngredients() {
-        return new Ingredient[0];
+
+        Ingredient[] a = new Ingredient[this._toppings.length + 3];
+        for (int i = 0; i < this._toppings.length; i++) {
+            a[i] = this._toppings[i];
+        }
+        a[this._toppings.length] = this._crust;
+        a[this._toppings.length + 1] = this._sauce;
+        a[this._toppings.length + 2] = this._cheese;
+        return a;
     }
 }
